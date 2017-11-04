0acf58fbb7631bd3377dd66595c03065537bd2be
emilyruby <zcabeme@ucl.ac.uk>
2017-11-04 15:42:30 +0000

    final changes

--- box.py
+++ box.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 
+
 def box(width, height):
-    for i in range(h):
-    
+
+    if width <= 1 or height <= 1:
+        return
+
+    result = ''
+
+    for i in range(height):
+
         start = middle = end = ""
 
         if i == 0:
             # first row
             start, middle, end = "Γ", "-", "˥"
         elif i == height - 1:
-            # last row
+            # last row
             start, middle, end = "L", "_", "┘"
         else:
             # anywhere but the first or last row
             start, middle, end = "|", " ", "|"
 
-        # draw the box with the start character, the appropriate number of middle characters, and the end character.
-        
-        print(start + middle * (width - 2) + end)
+        # draw the box with the start character, the appropriate number of
+        # middle characters, and the end character.
+
+        result += (start + middle * (width - 2) + end) + '\n'
+
+    return result
