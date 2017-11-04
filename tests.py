1bdfd060679acd323605c456a6afb91e8f6796c5
Emily Mears <zcabeme@ucl.ac.uk>
2017-11-04 13:17:40 +0000

    add test outline

---
+++ tests.py
@@ -0,0 +1,22 @@
+import unittest
+from treatwell import box
+
+
+class TestBox(unittest.TestCase):
+    def setUp(self):
+        self.data = []
+
+    def test_box_drawing(self):
+        self.assertEqual()
+
+    def test_box_drawing2(self):
+        self.assertEqual()
+
+    def test_zero_box(self):
+        self.assertEqual()
+
+    def test_large_input(self):
+        self.assertEqual()
+
+    def test_negative_input(self):
+        self.assertEqual()
